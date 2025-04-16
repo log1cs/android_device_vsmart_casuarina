29836d089950674243ab35cf5972f371f9ef053b
Tuan Anh <tuan73176@gmail.com>
2025-04-16 22:29:04 +0700

    casuarina: Inherit vendor makefiles
    Change-Id: Ic70c94560321de00cf16e19f85b6f91d829c8715

--- device.mk
+++ device.mk
@@ -2,3 +2,6 @@
 # SPDX-FileCopyrightText: 2025 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Inherit vendor makefiles
+$(call inherit-product, vendor/vsmart/casuarina/casuarina-vendor.mk)
