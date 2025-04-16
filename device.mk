b2422ec811c7104365a97c54a7c86d3d9d7a3575
Tuan Anh <tuan73176@gmail.com>
2025-04-16 22:36:24 +0700

    casuarina: Setup boot animation resolutions
    Change-Id: I8d9222c57b8952a091aaa7fd59b8c6fb4bc02de0

--- device.mk
+++ device.mk
@@ -3,5 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 720
+
 # Inherit vendor makefiles
 $(call inherit-product, vendor/vsmart/casuarina/casuarina-vendor.mk)
