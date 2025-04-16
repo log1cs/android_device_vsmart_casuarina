a3ad95aba994b44fc9cbc780e2aca1163319574d
jmpfbmx <jmpf.bmx@gmail.com>
2025-04-16 22:08:38 +0700

    casuarina: Add arch, bootloader and platform flags
    Change-Id: Ie0ca1d06a57a715867ceb4954cc37248cd099081
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,28 @@
 
 DEVICE_PATH := device/vsmart/casuarina
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8953
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := msm8953
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_casuarina_defconfig
