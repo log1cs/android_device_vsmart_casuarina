8365781bdcc272732c185e8740b138fc38eec517
Tuan Anh <tuan73176@gmail.com>
2025-04-17 11:17:17 +0700

    casuarina: Import device specific init scripts
    casuarina_open-user 10 QKQ1.200311.002 V430A_OPN_U_B15_211112 release-keys
    
    Change-Id: I9d3ac1c82030667ad14e340a90f7c3c91c88291b

--- device.mk
+++ device.mk
@@ -7,5 +7,19 @@
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 720
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.usb.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
+    init.msm.usb.configfs.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.recovery.qcom.rc \
+    init.target.rc \
+    ueventd.qcom.rc
+
 # Inherit vendor makefiles
 $(call inherit-product, vendor/vsmart/casuarina/casuarina-vendor.mk)
