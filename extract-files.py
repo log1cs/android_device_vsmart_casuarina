f1252d512522ee43357d6db87eafaded510d7796
Tuan Anh <tuan73176@gmail.com>
2025-04-17 11:21:42 +0700

    casuarina: Import LineageOS extract utils
    From LineageOS/android_tools_extract-utils@f2a475a21893533b06ee9143728d200c44907072.
    Strip out all unused stuff that the device don't use.
    
    Change-Id: If597551aa6b442a8cf882309e7c22b2888bcc73a

---
+++ extract-files.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_9'
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/vsmart/casuarina',
+    'hardware/qcom-caf/msm8996',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/dataservices',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+blob_fixups: blob_fixups_user_type = {
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'casuarina',
+    'vsmart',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
