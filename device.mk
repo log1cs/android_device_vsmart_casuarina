8be7a11a1111727fb7c0ec9c87dce6607ca6aa37
Jan Altensen <info@stricted.net>
2025-04-21 23:24:08 +0700

    casuarina: Set libfmjni variable to qcom
    Change-Id: Idb8bd733c9d229d97cd348215958c74acc5ea53d

--- device.mk
+++ device.mk
@@ -49,6 +49,9 @@ TARGET_SCREEN_WIDTH := 720
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# FM
+$(call soong_config_set,libfmjni,vendor,qcom)
+
 # IRQ balance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance_little_big.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_little_big.conf
