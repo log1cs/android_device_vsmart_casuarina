6fcd8c16fc22a3a8d3cbe5aa8223698391a67ead
Bruno Martins <bgcngm@gmail.com>
2025-04-21 23:07:40 +0700

    casuarina: Inherit from QTI FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_casuarina
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
