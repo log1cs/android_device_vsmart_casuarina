c4fa04fd0ac22964d00b5a4db7f6d6498e038c80
Michael Bestas <mkbestas@gmail.com>
2025-04-22 21:07:55 +0700

    casuarina: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- extract-files.py
+++ extract-files.py
@@ -52,6 +52,8 @@ blob_fixups: blob_fixups_user_type = {
         .fix_xml(),
     'vendor/bin/pm-service': blob_fixup()
         .add_needed('libutils-v33.so'),
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
