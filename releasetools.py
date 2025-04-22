0e5d2ce9823dcd0ac3894490358774db9f891d2d
Kuba Wojciechowski <nullbytepl@gmail.com>
2025-04-22 20:55:49 +0700

    casuarina: releasetools: Update firmware images
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Change-Id: I3c2fdc4a5941d3b4e6034f5fa1d2eaa1e158af81

--- releasetools.py
+++ releasetools.py
@@ -27,7 +27,28 @@ def AddImage(info, basename, dest):
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
+def AddImageRadio(info, basename, dest):
+  name = basename
+  if ("RADIO/" + basename) in info.input_zip.namelist():
+    data = info.input_zip.read("RADIO/" + basename)
+    common.ZipWriteStr(info.output_zip, name, data)
+    info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+    info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImageRadio(info, "aboot.mbn", "/dev/block/bootdevice/by-name/aboot")
+  AddImageRadio(info, "cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib")
+  AddImageRadio(info, "cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64")
+  AddImageRadio(info, "devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg")
+  AddImageRadio(info, "dsp.bin", "/dev/block/bootdevice/by-name/dsp")
+  AddImageRadio(info, "keymaster.mbn", "/dev/block/bootdevice/by-name/keymaster")
+  AddImageRadio(info, "lksecapp.mbn", "/dev/block/bootdevice/by-name/lksecapp")
+  AddImageRadio(info, "mdtp.mbn", "/dev/block/bootdevice/by-name/mdtp")
+  AddImageRadio(info, "modem.bin", "/dev/block/bootdevice/by-name/modem")
+  AddImageRadio(info, "rpm.mbn", "/dev/block/bootdevice/by-name/rpm")
+  AddImageRadio(info, "sbl1.mbn", "/dev/block/bootdevice/by-name/sbl1")
+  AddImageRadio(info, "splash.img", "/dev/block/bootdevice/by-name/splash")
+  AddImageRadio(info, "tz.mbn", "/dev/block/bootdevice/by-name/tz")
   return
