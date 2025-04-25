a12b837665c0c4ba604b62c3fc70f842bd6351d2
Bruno Martins <bgcngm@gmail.com>
2025-04-25 15:22:50 +0700

    casuarina: Build AIDL ClearKey DRM HAL
    [Tuan Anh: Keep the Widevine VINTF entries because we don't have a separated one]
    
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
