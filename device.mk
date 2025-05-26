3f2553bf11e7b458a1ce033e5083ed71fd4b2d07
EndCredits <endcredits@crepuscular-aosp.icu>
2025-05-26 23:52:28 +0700

    casuarina: Build sensors HAL
    Change-Id: I927e1b5779e324c70070c7fe58def21e3e0228dc
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- device.mk
+++ device.mk
@@ -204,6 +204,11 @@ PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
     WifiOverlay
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-service
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
