80c54eceff608f8ce8f2d762c25e54a3e5ccb96a
Luofan Chen <clfbbn@gmail.com>
2025-05-28 00:49:42 +0700

    casuarina: Build common Lineage fingerprint AIDL HAL
    Change-Id: I8bb34a7856846a5e34466b838ab24b6bd53ca623

--- device.mk
+++ device.mk
@@ -85,6 +85,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.lineage
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
