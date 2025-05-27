7b52c0481fd41f8170cd080c7f5c9139adee4c5d
LuK1337 <priv.luk@gmail.com>
2025-05-27 17:22:38 +0700

    casuarina: Build telephony packages
    Change-Id: Iaf6741035a559d4daecda5548a607f4b83c860db

--- device.mk
+++ device.mk
@@ -258,6 +258,17 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     bootable/deprecated-ota
 
+# Telephony
+PRODUCT_PACKAGES += \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Update
 AB_OTA_UPDATER := false
 
