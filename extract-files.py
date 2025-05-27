d805a4dbc1cb480c153cbf370cb9e840567bc7a3
Łukasz Patron <priv.luk@gmail.com>
2025-05-27 21:24:21 +0700

    casuarina: Clear LIBC_PRIVATE symbol versions for libmmcamera_faceproc
    Change-Id: I1565a6fef5070377650e62dc3e3a45bfd2bfaf5b

--- extract-files.py
+++ extract-files.py
@@ -51,6 +51,10 @@ blob_fixups: blob_fixups_user_type = {
 	.replace_needed('libqdMetaData.so', 'libqdMetaData.system.so'),
     'vendor/bin/pm-service': blob_fixup()
 	.add_needed('libutils-v33.so'),
+    ('vendor/lib/libmmcamera_faceproc.so', 'vendor/lib/libmmcamera_faceproc2.so'): blob_fixup()
+        .clear_symbol_version('__aeabi_memcpy')
+        .clear_symbol_version('__aeabi_memset')
+        .clear_symbol_version('__gnu_Unwind_Find_exidx'),
     'vendor/lib64/vendor.fpsensor.hardware.fpsensorhidlsvc@2.0.so': blob_fixup()
 	.replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
