0e1f62b46dd21e03bea5cf52b59d0505eff73763
Michael Bestas <mkbestas@lineageos.org>
2025-05-27 21:03:33 +0700

    casuarina: Enforce vintf kernel requirements
    The build system enables this for shipping API level >=29
    but we also want it to pass the vintf runtime checks.
    
    Change-Id: Ie2147893a1551b736880913334a3c134a17ca91c

--- device.mk
+++ device.mk
@@ -291,6 +291,9 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# Vintf
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
+
 # WLAN firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink \
