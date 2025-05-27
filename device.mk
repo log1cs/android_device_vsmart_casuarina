f29ce3186a63ced9826849f278582309c5080aee
Eric Meddaugh <eric@meddaughs.com>
2025-05-28 00:41:49 +0700

    casuarina: Uprev to audio HAL 6.0
    Change-Id: I8f4807d1e270554e4a3481321ece74cffcd7cc5f

--- device.mk
+++ device.mk
@@ -12,8 +12,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.1-impl \
     audio.primary.msm8953 \
