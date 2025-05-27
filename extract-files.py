89e8a16882a0858a5d1a240576d6f38d6b0e74eb
Alexander Martinz <amartinz@shiftphones.com>
2025-05-27 21:20:06 +0700

    casuarina: Use libhidlbase-v32 for fingerprint libs
    Change-Id: Iaef8ede57250aa171f9863e7e250c604ef241334

--- extract-files.py
+++ extract-files.py
@@ -53,6 +53,8 @@ blob_fixups: blob_fixups_user_type = {
 	.replace_needed('libqdMetaData.so', 'libqdMetaData.system.so'),
     'vendor/bin/pm-service': blob_fixup()
 	.add_needed('libutils-v33.so'),
+    'vendor/lib64/vendor.fpsensor.hardware.fpsensorhidlsvc@2.0.so': blob_fixup()
+	.replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
 }  # fmt: skip
