3e8122af40a7b27df32b56e415aad359ad30df85
Tim Zimmermann <tim@linux4.de>
2025-06-01 08:08:12 +0700

    casuarina: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- lineage_casuarina.mk
+++ lineage_casuarina.mk
@@ -6,6 +6,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Indicate the first API level the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 28
