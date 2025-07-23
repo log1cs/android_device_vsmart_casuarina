09b9b0a0fa05d9603d72b08bd4d9946ebdeb6d4b
0xCAFEBABE <me.cafebabe@gmail.com>
2025-07-23 22:06:32 +0700

    casuarina: Switch to QTI Vibrator AIDL
    It provides haptic effects with qti haptics driver,
    Meanwhile still supports simple vibration with qpnp haptics driver.
    
    Change-Id: I83d0c5f562c855afba563e7b959d3e0cd697ef26

--- device.mk
+++ device.mk
@@ -298,8 +298,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
 
 # Vintf
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
