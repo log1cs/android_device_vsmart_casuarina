6a45ff5e43765a67aa469be8912c15552f82413a
Tuan Anh <tuan73176@gmail.com>
2025-08-03 04:36:47 +0000

    casuarina: audio: Drop audio_effects.conf
    We're using the XML ones, and .conf audio effects has been long dead.
    
    Change-Id: Iff8cc669707963c41d3f47d615a350ab84871734

--- device.mk
+++ device.mk
@@ -27,7 +27,6 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
