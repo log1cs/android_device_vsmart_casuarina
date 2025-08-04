f5833006721c291308aa8d45544f3966002fb0d1
Ying Wang <wangying@google.com>
2025-08-04 22:50:52 +0000

    casuarina: camera: Cleanup Obsolete LOCAL_PRELINK_MODULE.
    Bug: 18675947
    Change-Id: Idbf25db61cecb2016a506b75028ce675f6e5294b

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -80,7 +80,6 @@ LOCAL_SRC_FILES := \
 
 LOCAL_MODULE           := libmmjpeg_interface
 include $(SDCLANG_COMMON_DEFS)
-LOCAL_PRELINK_MODULE   := false
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libqomx_core libmmcamera_interface
 ifeq ($(strip $(LIB2D_ROTATION)),true)
     LOCAL_SHARED_LIBRARIES += libmmlib2d_interface
